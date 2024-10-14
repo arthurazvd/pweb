257d5a1177ff0080d6e87804debc1061c7fe0e36
Arthur <azvd.arthur@gmail.com>
2024-10-14 17:43:56 -0300

    Receita 5 - Exercício 1

---
+++ HTML/RECEITA 5/script.js
@@ -0,0 +1,17 @@
+let cervejas = ["Guiness", "Desperados", "Becks"]    
+
+const carregarDiv = () => {
+
+    const div = document.getElementById("meu amor")
+
+    const cervejasHtml = cervejas.map( item => `<h1>${item}</h1>` ) 
+
+    div.innerHTML = `${cervejasHtml.join("\n")}`
+
+}
+
+
+
+let botao = document.getElementById("botaoCarregar")
+
+botao.addEventListener("click", carregarDiv)    
