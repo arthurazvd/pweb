f59d9fc0558fc8f5ad62acb07f6a49d6826538cd
Arthur <azvd.arthur@gmail.com>
2024-11-01 20:04:44 -0300

    RECEITA 5 - COMPLETA

--- HTML/RECEITA 5/script.js
+++ HTML/RECEITA 5/script.js
@@ -1,17 +1,85 @@
-let cervejas = ["Guiness", "Desperados", "Becks"]    
+let timesDeFutebol = [
+    "Flamengo", "Palmeiras", "São Paulo", "Corinthians", "Vasco", "Santos", "Grêmio", "Internacional", "Cruzeiro",
+    "Atlético Mineiro"
+];
 
-const carregarDiv = () => {
-
-    const div = document.getElementById("meu amor")
-
-    const cervejasHtml = cervejas.map( item => `<h1>${item}</h1>` ) 
-
-    div.innerHTML = `${cervejasHtml.join("\n")}`
-
+function transformar(item) {
+    return `
+       <tr>
+          <td> 
+             <div class="times">
+                <h1>${item}</h1>
+             </div>
+          </td>
+       </tr>
+    `;
 }
 
+function carregarTimes() {
+    let thead = document.getElementById("tabela-times").querySelector("thead");
+    let tbody = document.getElementById("tabela-times").querySelector("tbody");
 
+    thead.innerHTML = `
+       <tr>
+          <th>
+             <h1>
+                Times de Futebol
+             </h1>
+          </th>
+       </tr>
+    `;
 
-let botao = document.getElementById("botaoCarregar")
+    let timesHtml = timesDeFutebol.map(transformar);
+    tbody.innerHTML = `${timesHtml.join("\n")}`;
 
-botao.addEventListener("click", carregarDiv)    
+    document.getElementById("botaoMostrarOrdenado").style.display = "none";
+    document.getElementById("botaoCarregar").style.display = "none";
+}
+
+function mostrarOrdenadamente() {
+    let thead = document.getElementById("tabela-times").querySelector("thead");
+    let tbody = document.getElementById("tabela-times").querySelector("tbody");
+
+    thead.innerHTML = `
+       <tr>
+          <th>
+             <h1>
+                Times de Futebol
+             </h1>
+          </th>
+       </tr>
+    `;
+
+    let timesOrdenados = [...timesDeFutebol].sort();
+    let timesHtml = timesOrdenados.map(transformar);
+    tbody.innerHTML = `${timesHtml.join("\n")}`;
+
+    document.getElementById("botaoMostrarOrdenado").style.display = "none";
+    document.getElementById("botaoCarregar").style.display = "none";
+}
+
+function embaralharTimes() {
+    let thead = document.getElementById("tabela-times").querySelector("thead");
+    let tbody = document.getElementById("tabela-times").querySelector("tbody");
+
+    thead.innerHTML = `
+       <tr>
+          <th>
+             <h1>
+                Times de Futebol
+             </h1>
+          </th>
+       </tr>
+    `;
+
+    let timesEmbaralhados = [...timesDeFutebol].sort(() => Math.random() - 0.5);
+    let timesHtml = timesEmbaralhados.map(transformar);
+    tbody.innerHTML = `${timesHtml.join("\n")}`;
+
+    document.getElementById("botaoMostrarOrdenado").style.display = "none";
+    document.getElementById("botaoCarregar").style.display = "none";
+}
+
+document.getElementById("botaoCarregar").addEventListener("click", carregarTimes); 
+document.getElementById("botaoMostrarOrdenado").addEventListener("click", mostrarOrdenadamente);
+document.getElementById("botaoEmbaralhar").addEventListener("click", embaralharTimes);
